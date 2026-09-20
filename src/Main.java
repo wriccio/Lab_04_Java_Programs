53f4ff70c76c818861733d2196f2840ee28135d2
William Riccio <ricciowl@mail.uc.edu>
2026-09-20 09:53:58 -0500

    saving info

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) { // 2. Opens the method
+        // Variable Declarations and Initialization
+        double vehiclePrice = 39500.00;
+        double taxRate = 0.05;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
-}
+        // Calculations
+        double salesTax = vehiclePrice * taxRate;
+        double totalPrice = vehiclePrice + salesTax;
+
+        // Output Statements
+        System.out.println("To Purchase a Toyota Camry Hybrid Nightshade Edition");
+        System.out.println("The price of the vehicle is: $" + vehiclePrice);
+        System.out.println("5% sales tax is: $" + salesTax);
+        System.out.println("Total price including tax is: $" + totalPrice);
+    } // 2. Closes the method
+} // 1. Closes the class (Check if this line is missing in your file!)
